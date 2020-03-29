d121245de4cbf6d57b12c921f10b27f44340acb6
Mrzhou-hr <211535176@qq.com>
2020-03-29 15:17:20 +0800

    ajax提交表单数据及二进制数据的formdata对象

--- serve/app.js
+++ serve/app.js
@@ -1,6 +1,8 @@
 /* ========= node.js静态服务程序(依赖nodejs环境及nodejs系统模块搭建)=========== */
 const http = require("http"); //导入nodejs的系统http模块用于创建服务
 const url = require("url"); //导入nodejs的系统url模块用于解析请求地址
+const formidable = require("formidable"); //解析formdata对象参数及二进制参数模块
+const path = require("path");
 const app = http.createServer();
 let isok = "cg";
 app.on("request", (req, res) => {
@@ -12,7 +14,7 @@ app.on("request", (req, res) => {
     "Access-Control-Allow-Headers": "X-Requested-With, Content-Type"
   });
   var urlparse = url.parse(req.url, true);
-  console.log(urlparse, req.method);
+  //console.log(urlparse, req.method);
 
   //判断客户端的请求方式(该服务只用于客户端测试ajax)
   if (req.method == "GET") {
@@ -86,11 +88,22 @@ app.on("request", (req, res) => {
           }
         });
         break;
+      case "/formdata":
+        const form = new formidable.IncomingForm(); //创建解析formdata对象(formidable Node.js第三方框架)
+        form.uploadDir = path.join(__dirname, "public", "uploads"); //设置上传的二进制文件保存路径
+        form.keepExtensions = true; //设置上传文件保存时是否保留后缀名（默认不保存）
+        form.parse(req, (err, fields, files) => {
+          console.log(fields); //解析的普通表单文件或者（formdata对象文件）
+          console.log(files); //二进制上传文件的解析后信息
+          res.end(
+            "服务器接收到你的请求，发送请求的参数为" + JSON.stringify(fields)
+          );
+        });
       default:
         break;
     }
   }
 });
-app.listen(80, () => {
+app.listen(8080, () => {
   console.log("服务启动成功请访问");
 });
